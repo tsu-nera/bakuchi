f97326ac53574ab8c5fc37487b50509531c14c7d
tsu-nera <fox10225fox@gmail.com>
2020-03-10 00:12:04 +0900

    wip

--- src/libs/ccxt_client.py
+++ src/libs/ccxt_client.py
@@ -13,26 +13,30 @@ class CcxtClient():
         self.exchange = eval('ccxt.{}()'.format(exchange_id))
         self.symbol = ccxtconst.SYMBOL_BTC_JPY  # とりあえず BTC/JPY固定
 
-    def _exec(self, proc):
+    def _exec(self):
         try:
-            return proc()
-        except ccxt.ExchangeNotAvailable:
-            self.logger.exception("exchange not available error occured")
+            return self.exchange.fetch_ticker(self.symbol)
+        except ccxt.ExchangeNotAvailable as e:
+            self.logger.error(e)
+            self.logger.errro("exchange not available error occured")
             return None
-        except ccxt.RequestTimeout:
-            self.logger.exception("timeout error occured")
-        except ccxt.RateLimitExceeded:
-            self.logger.exception("rate limit error occured. exit.")
+        except ccxt.RequestTimeout as e:
+            self.logger.error(e)
+            self.logger.error("timeout error occured")
+            return None
+        except ccxt.RateLimitExceeded as e:
+            self.logger.error(e)
+            self.logger.error("rate limit error occured. exit.")
             sys.exit(1)
         except Exception as e:
-            self.logger.exception("error occourd", e)
-        return None
+            self.logger.exception(e)
+            self.logger.error("error occourd")
+            return None
 
     def fetch_tick(self):
         date_string = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
-        proc = lambda: self.exchange.fetch_ticker(self.symbol)
-        tick = self._exec(proc)
+        tick = self._exec()
 
         return {
             "date": date_string,
