bba49f4e6b83782b29347f18014dba693abeabff
tsu-nera <fox10225fox@gmail.com>
2020-03-13 11:54:18 +0900

    サーバダウン対応

--- src/utils/public.py
+++ src/utils/public.py
@@ -7,8 +7,14 @@ def fetch_ticks(exchange_id, symbol=ccxtconst.SYMBOL_BTC_JPY):
     client = CcxtClient(exchange_id, symbol)
 
     while True:
+        time.sleep(1)
+        
         tick = client.fetch_tick()
+        
+        if not tick:
+            print("no data...")
+            continue
+        
         output = "{} bid={} ask={}".format(tick["timestamp"], tick["bid"],
                                            tick["ask"])
         print(output)
-        time.sleep(1)
