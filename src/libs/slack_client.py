f3467ed8116d03b0272713b0386a039c3ee95062
tsu-nera <fox10225fox@gmail.com>
2020-03-14 16:13:01 +0900

    update

--- src/libs/slack_client.py
+++ src/libs/slack_client.py
@@ -2,17 +2,17 @@ import slack
 from src.env import SLACK_API_TOKEN
 
 BOT_CHANNEL_NAME = "#bakuchi-bot"
+USER_NAME = "trading bot"
+SLACK_ICON_URL = "https://github.com/tsu-nera/bakuchi/blob/master/images/bitcoin.png"
 
 
 class SlackClient():
     def __init__(self):
         self.client = slack.WebClient(token=SLACK_API_TOKEN)
 
-        self.channel = BOT_CHANNEL_NAME
-        self.user_name = "trading bot"
-
     def notify(self, message):
-        self.client.chat_postMessage(channel=self.channel,
+        self.client.chat_postMessage(channel=BOT_CHANNEL_NAME,
                                      as_user=False,
-                                     username=self.user_name,
+                                     username=USER_NAME,
+                                     icon_url=SLACK_ICON_URL,
                                      text=message)
