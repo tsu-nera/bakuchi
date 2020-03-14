c86e55fad5ac9906c809b4ffe7e1679ffd3241ef
tsu-nera <fox10225fox@gmail.com>
2020-03-14 12:10:10 +0900

    feat: logging price difference

--- src/core/arbitrage_trading.py
+++ src/core/arbitrage_trading.py
@@ -10,6 +10,8 @@ from src.config import config
 from src.libs.logger import get_trading_logger
 from src.libs.logger import get_trading_logger_with_stdout
 
+from src.libs.logger import get_margin_logger
+
 
 class ArbitrageTrading(ArbitrageBase):
     def __init__(self, exchange_id_x, exchange_id_y, symbol):
@@ -31,18 +33,33 @@ class ArbitrageTrading(ArbitrageBase):
 
         self.logger = get_trading_logger()
         self.logger_with_stdout = get_trading_logger_with_stdout()
+        self.logger_margin = get_margin_logger()
 
         self.trade_amount = float(config["trade"]["amount"])
         self.profit_margin_threshold = int(
             config["trade"]["profit_margin_threshold"])
 
     def run(self):
-        print("btc amount: {}".format(self.trade_amount))
-        print("profit margin threshold: {}".format(
-            self.profit_margin_threshold))
+        self.logger_with_stdout.info(
+            "amount={}, profit_margin_threshold={}".format(
+                self.trade_amount, self.profit_margin_threshold))
+
         while True:
             sleep(TICK_INTERVAL_SEC)
             self.next()
+
+    def _logging_tick_margin(self, x, y):
+        if not x or not y:
+            return
+
+        buy_y_sell_x_margin = x.bid - y.ask
+        buy_x_sell_y_margin = y.bid - x.ask
+
+        message = "sell[{}] buy[{}] margin={}, sell[{}] buy[{}] margin={}, action_permission={}".format(
+            self.ex_id_x, self.ex_id_y, buy_y_sell_x_margin, self.ex_id_y,
+            self.ex_id_x, buy_x_sell_y_margin, self.action_permission)
+
+        self.logger_margin.info(message)
 
     def _get_tick(self):
         x = self.client_x.fetch_tick()
@@ -50,6 +67,8 @@ class ArbitrageTrading(ArbitrageBase):
 
         tick_x = Tick(x["timestamp"], x["bid"], x["ask"]) if x else None
         tick_y = Tick(y["timestamp"], y["bid"], y["ask"]) if y else None
+
+        self._logging_tick_margin(tick_x, tick_y)
 
         return tick_x, tick_y
 
