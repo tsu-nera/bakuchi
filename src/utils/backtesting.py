1a4bc2e2a97caa4d1b252baf5301c2b8495a2c7b
tsu-nera <fox10225fox@gmail.com>
2020-03-15 12:48:02 +0900

    reactor: backtesting tools

--- src/utils/backtesting.py
+++ src/utils/backtesting.py
@@ -6,39 +6,46 @@ from src.drivers.csv_driver import CsvDriver
 from src.core.arbitrage_backtesting import ArbitrageBacktesting
 
 
-def _get_file_path(timestamp, exchange_id):
-    file_name = "{}.csv".format(exchange_id)
-    return os.path.join(BACKTEST_DATA_DIR_PATH, timestamp, file_name)
+class Backtesting():
+    def __init__(self, timestamp):
+        self.timestamp = timestamp
+        self.csv_driver = CsvDriver()
+
+        self.df_cc = self._read_df(ccxtconst.EXCHANGE_ID_COINCHECK)
+        self.df_lq = self._read_df(ccxtconst.EXCHANGE_ID_LIQUID)
+
+    def _get_file_path(self, exchange_id):
+        file_name = "{}.csv".format(exchange_id)
+        return os.path.join(BACKTEST_DATA_DIR_PATH, self.timestamp, file_name)
+
+    def _read_df(self, exchange_id):
+        path = self._get_file_path(exchange_id)
+        return self.csv_driver.read_df(path)
+
+    def run(self):
+        print("=== backtest start ===")
+        print()
+
+        # run trade
+        arbitrage = ArbitrageBacktesting(self.df_cc, self.df_lq,
+                                         ccxtconst.SYMBOL_BTC_JPY,
+                                         ccxtconst.EXCHANGE_ID_COINCHECK,
+                                         ccxtconst.EXCHANGE_ID_LIQUID)
+        arbitrage.run()
+
+        # show result
+        arbitrage.report()
+
+        print()
+        print("=== backtest end ===")
+
+    def get_coincheck_df(self):
+        return self.df_cc
+
+    def get_liquid_df(self):
+        return self.df_lq
 
 
 def run_backtesting(timestamp):
-    print("=== backtest start ===")
-    print()
-
-    # load dataset
-    df_cc = read_coincheck_df(timestamp)
-    df_lq = read_liquid_df(timestamp)
-
-    # run trade
-    arbitrage = ArbitrageBacktesting(df_cc, df_lq, ccxtconst.SYMBOL_BTC_JPY,
-                                     ccxtconst.EXCHANGE_ID_COINCHECK,
-                                     ccxtconst.EXCHANGE_ID_LIQUID)
-    arbitrage.run()
-
-    # show result
-    arbitrage.report()
-
-    print()
-    print("=== backtest end ===")
-
-
-def read_coincheck_df(timestamp):
-    path = _get_file_path(timestamp, ccxtconst.EXCHANGE_ID_COINCHECK)
-    csv_driver = CsvDriver()
-    return csv_driver.read_df(path)
-
-
-def read_liquid_df(timestamp):
-    path = _get_file_path(timestamp, ccxtconst.EXCHANGE_ID_LIQUID)
-    csv_driver = CsvDriver()
-    return csv_driver.read_df(path)
+    backtest = Backtesting(timestamp)
+    backtest.run()
