2b2415037e05e4f50d5b1864350a11860f9308b6
tsu-nera <fox10225fox@gmail.com>
2020-03-20 02:05:57 +0900

    update

--- bin/trading_session.sh
+++ bin/trading_session.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 tmux new-session -d -s trading
 tmux split-window -v "tail -f logs/cron/asset.log"
-tmux split-window -v "tail -f logs/margin.log"
-tmux split-window -v "tail -f logs/ccxt.log"
+tmux split-window -v "tail -f logs/trades/margin.log"
+tmux split-window -v "tail -f logs/trades/ccxt.log"
 tmux attach -t trading
