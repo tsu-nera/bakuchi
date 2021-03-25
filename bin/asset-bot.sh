4110856710023ec9c048cb94f802ce78441e6fd2
tsu-nera <fox10225fox@gmail.com>
2021-03-25 17:14:43 +0900

    feat: asset-bot.shの修正

--- bin/asset-bot.sh
+++ bin/asset-bot.sh
@@ -12,8 +12,11 @@ inv bot-asset
 ASSET_FILE_FROM=${EXECDIR}/logs/cron/asset.log
 ASSET_FILE_TO=${SAVEDIR}/logs/cron/asset.log
 
-ASSET_FILE_FROM_COINCHECK=${EXECDIR}/data/assets/coincheck.csv
-ASSET_FILE_TO_COINCHECK=${SAVEDIR}/data/assets/coincheck.csv
+# ASSET_FILE_FROM_COINCHECK=${EXECDIR}/data/assets/coincheck.csv
+# ASSET_FILE_TO_COINCHECK=${SAVEDIR}/data/assets/coincheck.csv
+
+ASSET_FILE_FROM_BITBANK=${EXECDIR}/data/assets/bitbank.csv
+ASSET_FILE_TO_BITBANK=${SAVEDIR}/data/assets/bitbank.csv
 
 ASSET_FILE_FROM_LIQUID=${EXECDIR}/data/assets/liquid.csv
 ASSET_FILE_TO_LIQUID=${SAVEDIR}/data/assets/liquid.csv
@@ -22,6 +25,6 @@ ASSET_FILE_FROM_TOTAL=${EXECDIR}/data/assets/total.csv
 ASSET_FILE_TO_TOTAL=${SAVEDIR}/data/assets/total.csv
 
 cp ${ASSET_FILE_FROM} ${ASSET_FILE_TO}
-cp ${ASSET_FILE_FROM_COINCHECK} ${ASSET_FILE_TO_COINCHECK}
+cp ${ASSET_FILE_FROM_BITBANK} ${ASSET_FILE_TO_BITBANK}
 cp ${ASSET_FILE_FROM_LIQUID} ${ASSET_FILE_TO_LIQUID}
 cp ${ASSET_FILE_FROM_TOTAL} ${ASSET_FILE_TO_TOTAL}
